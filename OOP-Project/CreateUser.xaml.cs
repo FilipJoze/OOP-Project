e3f5f2bbde7e1471b1254852bb40a0cdac83d593
FilipJoze <filipjoze@gmail.com>
2022-04-07 09:35:39 +0100

    Second half of the SQL connection updated plus Panel updates

--- OOP-Project/CreateUser.xaml.cs
+++ OOP-Project/CreateUser.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DAL;
+using BIZ;
 
 namespace OOP_Project
 {
@@ -19,6 +21,9 @@ namespace OOP_Project
     /// </summary>
     public partial class CreateUser : Window
     {
+        CreateUserAccount cua = new CreateUserAccount();
+        HashPass hp = new HashPass();
+
         public CreateUser()
         {
             InitializeComponent();
@@ -29,5 +34,92 @@ namespace OOP_Project
 
             this.Close();
         }
+
+        private void pbPasswordCheck_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            //bool for valid password 
+            bool passwordValid = false;
+
+            //rules for password
+            int minUpper = 1;
+            int minLower;
+            int minLength = 7;
+            string allowedSpecials = "@#/.!";
+            //gap
+            //
+            //
+            //
+            //
+            //
+
+            string createuser = txtUsername.Text;
+
+            string createpassword = pbPassword.Password.ToString();
+            char[] characters = createpassword.ToCharArray();
+            //current variables
+            int upper = 0;
+            int character = 0;
+            int length = createpassword.Length;
+            int illegalCharacters = 0;
+
+            //checks the password
+            foreach (char enteredCharacters in characters)
+            {
+                if (char.IsUpper(enteredCharacters))
+                {
+                    upper = upper + 1;
+
+                }
+                else if (char.IsLower(enteredCharacters))
+                {
+
+                }
+                else if (allowedSpecials.Contains(enteredCharacters.ToString())) //needs double check
+                {
+                    character = character + 1;
+
+                }
+                else
+                {
+                    illegalCharacters = illegalCharacters + 1;
+
+                }
+
+            }
+            if (upper < minUpper || length < minLength || illegalCharacters >= 1)
+            {
+                MessageBox.Show("Insufficient Password Criteria", "Re-Enter Password", MessageBoxButton.OK);
+                passwordValid = false;
+            }
+            else
+            {
+                MessageBox.Show("Password Criteria Met", "Password Choice Good", MessageBoxButton.OK);
+                passwordValid = true;
+                createpassword = hp.PassHash(pbPassword.Password.ToString()); //encrypts the accepted password under the criterias
+                cua.CreateUser(txtUsername.Text, createpassword);
+
+                MessageBox.Show("Admin Created", "Information", MessageBoxButton.OK);
+
+                txtUsername.Clear();
+                pbPassword.Clear();
+                pbPasswordCheck.Clear();
+
+            }
+            if (pbPasswordCheck.Password.ToString() != "")
+            {
+                if (passwordValid)
+                {
+                    txtStatus.Text = "";
+
+                }
+                else
+                {
+                    txtStatus.Text = "Invalid Password";
+                }
+            }
+
+        }
+
     }
 }
+
