3f3beb40f462773efcb11f094a2e8ee7f9a8a598
FilipJoze <filipjoze@gmail.com>
2022-04-07 19:52:18 +0100

    Deposit Withdraw SQL change

---
+++ DAL/Deposit_Withdraw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Deposit_Withdraw
+    {
+        DAO dao = new DAO();
+        public void UpdateDepositDetails(decimal bal, decimal am, int accno)
+        {
+            SqlCommand cmd = dao.OpenCon().CreateCommand();
+            cmd.CommandText = "uspUpdateTransfer";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@accno", accno);
+            cmd.Parameters.AddWithValue("@na", bal);
+            cmd.Parameters.AddWithValue("@am", am);
+
+            cmd.ExecuteNonQuery();
+            dao.CloseCon();
+        }
+
+    }
+}
